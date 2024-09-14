6d284981423b72854e3ccd21374bdd96f963c3ec
Robert Ungureanu <robertgamingclash@gmail.com>
2024-09-14 10:24:39 +0300

    Changed the project structure and ready to start the API work

--- cmd/app.go
+++ cmd/app.go
@@ -3,8 +3,8 @@ package main
 import (
 	"log"
 
-	"github.com/diother/go-invoices/models"
-	"github.com/diother/go-invoices/views"
+	"github.com/diother/go-invoices/internal/models"
+	"github.com/diother/go-invoices/internal/pdf"
 )
 
 func main() {
@@ -21,27 +21,9 @@ func main() {
 			{ProductName: "Donation to Project B", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN2", Gross: "150.00 lei", StripeFee: "15.00 lei", Total: "135.00 lei"},
 			{ProductName: "Charity Auction Item", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN3", Gross: "200.00 lei", StripeFee: "20.00 lei", Total: "180.00 lei"},
 			{ProductName: "Monthly Subscription", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN4", Gross: "300.00 lei", StripeFee: "30.00 lei", Total: "270.00 lei"},
-			{ProductName: "Fundraiser Gala Ticket", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN5", Gross: "250.00 lei", StripeFee: "25.00 lei", Total: "225.00 lei"},
-			{ProductName: "Merchandise Sale", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN6", Gross: "100.00 lei", StripeFee: "10.00 lei", Total: "90.00 lei"},
-			{ProductName: "Event Sponsorship", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN7", Gross: "400.00 lei", StripeFee: "40.00 lei", Total: "360.00 lei"},
-			{ProductName: "Online Course Enrollment", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN8", Gross: "120.00 lei", StripeFee: "12.00 lei", Total: "108.00 lei"},
-			{ProductName: "Workshop Registration", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN9", Gross: "80.00 lei", StripeFee: "8.00 lei", Total: "72.00 lei"},
-			{ProductName: "Donation to Health Fund", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN0", Gross: "90.00 lei", StripeFee: "9.00 lei", Total: "81.00 lei"},
-			{ProductName: "Holiday Appeal", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN10", Gross: "110.00 lei", StripeFee: "11.00 lei", Total: "99.00 lei"},
-			{ProductName: "Community Support", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN11", Gross: "130.00 lei", StripeFee: "13.00 lei", Total: "117.00 lei"},
-			{ProductName: "Animal Welfare Donation", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN12", Gross: "160.00 lei", StripeFee: "16.00 lei", Total: "144.00 lei"},
-			{ProductName: "Scholarship Fund", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN13", Gross: "180.00 lei", StripeFee: "18.00 lei", Total: "162.00 lei"},
-			{ProductName: "Environmental Project", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN14", Gross: "140.00 lei", StripeFee: "14.00 lei", Total: "126.00 lei"},
-			{ProductName: "Cultural Event Donation", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN15", Gross: "170.00 lei", StripeFee: "17.00 lei", Total: "153.00 lei"},
-			{ProductName: "Support for Local Artists", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN16", Gross: "200.00 lei", StripeFee: "20.00 lei", Total: "180.00 lei"},
-			{ProductName: "Donation to Project A", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN1", Gross: "100.00 lei", StripeFee: "10.00 lei", Total: "90.00 lei"},
-			{ProductName: "Donation to Project B", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN2", Gross: "150.00 lei", StripeFee: "15.00 lei", Total: "135.00 lei"},
-			{ProductName: "Public Health Campaign", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN17", Gross: "190.00 lei", StripeFee: "19.00 lei", Total: "171.00 lei"},
-			{ProductName: "Emergency Relief Fund", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN18", Gross: "210.00 lei", StripeFee: "21.00 lei", Total: "189.00 lei"},
-			{ProductName: "Education Support", TransactionId: "ch_1PZ0rmDXCtuWOFq8n33WSnN19", Gross: "230.00 lei", StripeFee: "23.00 lei", Total: "207.00 lei"},
 		},
 	}
-	payout := views.PayoutView{Payout: &pay}
+	payout := pdf.PayoutPdf{Payout: &pay}
 	err := payout.GenerateDocument()
 
 	// mon := models.MonthlyPayout{
@@ -89,7 +71,7 @@ func main() {
 	// 		},
 	// 	},
 	// }
-	// monthlyPayout := views.MonthlyPayoutView{MonthlyPayout: &mon}
+	// monthlyPayout := pdf.MonthlyPayoutPdf{MonthlyPayout: &mon}
 	// err := monthlyPayout.GenerateDocument()
 
 	// inv := models.Invoice{
@@ -100,7 +82,7 @@ func main() {
 	// 	UnitPrice:     "10.00 lei",
 	// 	Total:         "10.00 lei",
 	// }
-	// invoice := views.InvoiceView{Invoice: &inv}
+	// invoice := pdf.InvoicePdf{Invoice: &inv}
 	// err := invoice.GenerateDocument()
 
 	if err != nil {
