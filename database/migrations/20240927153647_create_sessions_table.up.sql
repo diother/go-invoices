cc2ec04b70b89f820e62eca82d82b0a72b23ef6b
Robert Ungureanu <robertgamingclash@gmail.com>
2024-10-03 19:47:03 +0300

    fixed migration sessions foreign key

--- database/migrations/20240927153647_create_sessions_table.up.sql
+++ database/migrations/20240927153647_create_sessions_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE sessions (
     session_token BIGINT PRIMARY KEY,
-    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE NOT NULL,
-    expires_at BIGINT NOT NULL
+    user_id BIGINT NOT NULL,
+    expires_at BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
